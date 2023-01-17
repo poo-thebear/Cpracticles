a18348800fc79a068cd85c57d4e4e8b3a4bb2056
ROSH <95622188+roshnirajak@users.noreply.github.com>
2023-01-17 19:28:56 +0530

    Update struct_1.c

--- struct_1.c
+++ struct_1.c
@@ -5,3 +5,13 @@ struct employ{
     char name[50], dep[50];
 };
 typedef struct employ emp;
+
+void main(){
+    emp a;
+
+    printf("Enter name: ");
+    scanf("%d", a.age);
+
+    printf("%s", a.age);
+
+}
